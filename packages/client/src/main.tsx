61caeed22722bfff4008f574244cd0ffe9f804d3
Ben Rabi <benr@wix.com>
2023-07-16 13:50:54 +0300

    add(): init cleanup

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -3,32 +3,26 @@ import ReactDOM from 'react-dom/client';
 
 const App = () => {
     const [data, setData] = React.useState('');
-  
     React.useEffect(() => {
         (async () => {
-          try {
-            console.log('1')
+        try {
             const response = await fetch('http://localhost:3001/api/data');
-            console.log({response})
             const data = await response.json();
-            console.log({ data });
             setData(data.message);
-          } catch (error) {
+        } catch (error) {
             console.error(error);
-          }
+        }
         })();
-      }, []);
+    }, []);
     
-  
     return <div>
             <section>Server Data</section>
                 {data}
         </div>;
-  };
+};
 
-  const rootElement = document.getElementById('root') as Element;
-  const app = <App />; // Replace App with your root component
-  
-  
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(app);
+const rootElement = document.getElementById('root') as Element;
+const app = <App />; // Replace App with your root component
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(app);
